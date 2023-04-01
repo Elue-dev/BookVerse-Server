0b88894f545892adc73089630cdd2a69df293dee
Wisdom <eluewisdom@gmail.com>
2023-04-01 09:41:33 +0100

    feat: slugified book title and added new column to db table

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,3 +1,4 @@
+const { default: slugify } = require("slugify");
 const catchAsync = require("../helpers/catchAsync");
 const GlobalError = require("../helpers/error.handler");
 const postgres = require("../postgres");
@@ -7,21 +8,28 @@ exports.addBook = catchAsync(async (req, res, next) => {
   const cacheKey = "allBooks";
   const cachedBooks = await retrieveRedisCache(cacheKey);
 
-  const redisCachedData = cachedBooks ? JSON.parse(cachedBooks) : [];
+  // const cachedBooks = cachedBooks ? JSON.parse(cachedBooks) : [];
+
+  const slugifiedText = slugify(req.body.title, {
+    lower: true,
+  });
 
   const sqlQuery =
-    "INSERT INTO books (title, description, price, userid) VALUES ($1, $2, $3, $4) RETURNING *";
+    "INSERT INTO books (title, description, price, bookimg, userid, slug, userimg) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *";
   const values = [
     req.body.title,
     req.body.description,
     req.body.price,
+    req.body.bookImg,
     req.user.id,
+    slugifiedText,
+    req.user.img,
   ];
 
   postgres.query(sqlQuery, values, async (err, book) => {
     if (err) return next(new GlobalError(err, 500));
 
-    const updatedRedisCache = [...redisCachedData, book.rows[0]];
+    const updatedRedisCache = [...cachedBooks, book.rows[0]];
 
     await redisClient.set(
       cacheKey,
@@ -30,7 +38,7 @@ exports.addBook = catchAsync(async (req, res, next) => {
       10
     );
 
-    res.status(201).json(book.rows[0]);
+    res.status(201).json({ message: "Book added", book: book.rows[0] });
   });
 });
 
@@ -50,14 +58,9 @@ exports.getAllBooks = catchAsync(async (req, res, next) => {
     postgres.query(sqlQuery, async (err, books) => {
       if (err) return next(new GlobalError(err, 500));
 
-      console.log(books.rows);
-
       await redisClient.set(cacheKey, JSON.stringify(books.rows), "EX", 10);
 
-      res.status(200).json({
-        results: books.rows.length,
-        books: books.rows,
-      });
+      res.status(200).json({ esults: books.rows.length, books: books.rows });
     });
   }
 });
@@ -139,10 +142,9 @@ exports.updateBook = catchAsync(async (req, res, next) => {
       10
     );
 
-    res.status(200).json({
-      message: "Book updated",
-      updatedBook: book.rows[0],
-    });
+    res
+      .status(200)
+      .json({ message: "Book updated", updatedBook: book.rows[0] });
   });
 });
 
@@ -159,7 +161,6 @@ exports.deleteBook = catchAsync(async (req, res, next) => {
   const sqlQuery = "DELETE FROM books WHERE id = $1 RETURNING *";
 
   postgres.query(sqlQuery, [req.params.bookId], async (err, book) => {
-    console.log(book);
     if (err) return next(new GlobalError(err, 500));
     if (book.rows.length === 0)
       return res
@@ -177,6 +178,6 @@ exports.deleteBook = catchAsync(async (req, res, next) => {
     await redisClient.del(userBookCacheKey);
     await redisClient.del(singleBookCacheKey);
 
-    res.status(200).json(`Book has been deleted`);
+    res.status(200).json(`Book deleted`);
   });
 });
