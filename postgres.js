7be8698577c1297e7fd96e60ef2db9b78f069ae4
Wisdom <eluewisdom@gmail.com>
2023-04-07 21:30:12 +0100

    patch: add back ssl

--- postgres.js
+++ postgres.js
@@ -5,6 +5,9 @@ let postgres;
 if (process.env.NODE_ENV === "production") {
   postgres = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 } else {
   postgres = new Pool({
